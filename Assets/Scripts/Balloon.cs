f249cd26ce41c5d193bc600d02837e69f9e0d630
KevinSilva <kevinsq16@gmail.com>
2014-10-27 03:03:46 -0400

    added color balloons

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -1,29 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Balloon : MonoBehaviour {
-
-    public int initialBalloons;
+public class Balloon : MonoBehaviour
+{
     public float initialSpeed;
     public Balloon balloonPrefab;
     public float sizeDecrement;
     public bool isAlive;
-    
-    private int currentBalloons;
+    public int balloonsInCluster;
+
+    //private int balloonsInCluster;
     private Vector3 direction;
     private PlayerCharacter player;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         isAlive = true;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCharacter>();
-        currentBalloons = initialBalloons;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        Color newColor = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), 1f);
+        gameObject.GetComponentInChildren<SpriteRenderer>().color = newColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -42,9 +45,9 @@ public class Balloon : MonoBehaviour {
 
     void Pop()
     {
-        if (currentBalloons == 1)
+        if (balloonsInCluster == 1)
         {
-            Die();   
+            Die();
         }
         else
         {
@@ -63,10 +66,12 @@ public class Balloon : MonoBehaviour {
     private void CreateNewBalloon(float rotationDegrees)
     {
         Balloon newBalloon = (Balloon)Instantiate(balloonPrefab, transform.position, Quaternion.identity);
+        newBalloon.name = "BalloonPrefab";
         newBalloon.transform.localScale *= sizeDecrement;
         newBalloon.isAlive = true;
+        newBalloon.balloonsInCluster = this.balloonsInCluster / 2;
         LinearFlight flight = newBalloon.GetComponent<LinearFlight>();
         flight.IncrementSpeed();
-        flight.RotateFlightDirection(rotationDegrees);        
+        flight.RotateFlightDirection(rotationDegrees);
     }
 }
